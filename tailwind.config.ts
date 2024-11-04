9047d0cc4bc751969f5d2709a85d8fe2b341dc2f
Yongho Kim <kyhcraft@gmail.com>
2024-11-04 17:35:50 +0900

    📦 init: 프로젝트 기본 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+    content: [
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/views/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {},
+    plugins: [],
 };
 export default config;
