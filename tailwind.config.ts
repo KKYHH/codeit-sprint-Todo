e07a3da4a303f8743497add343454167551c5669
Yongho Kim <kyhcraft@gmail.com>
2024-11-05 18:48:29 +0900

    feat: 컬러 시스템 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,30 @@
 import type { Config } from 'tailwindcss';
 
+export const Colors = {
+    slate: {
+        100: '#F1F5F9',
+        200: '#E2E8F0',
+        300: '#CBD5E1',
+        400: '#94A3B8',
+        500: '#64748B',
+        800: '#1E293B',
+        900: '#0F172A',
+    },
+    violet: {
+        100: '#EDE9FE',
+        600: '#7C3AED',
+    },
+    rose: {
+        500: '#F43F5E',
+    },
+    lime: {
+        300: '#BEF264',
+    },
+    amber: {
+        800: '#92400E',
+    },
+} as const;
+
 const config: Config = {
     content: [
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,7 +33,11 @@ const config: Config = {
         './src/views/**/*.{js,ts,jsx,tsx,mdx}',
         './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
     ],
-    theme: {},
+    theme: {
+        extend: {
+            colors: Colors,
+        },
+    },
     plugins: [],
 };
 export default config;
