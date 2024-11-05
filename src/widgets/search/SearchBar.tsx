d680d4f9af4fb1f2fa794b3b3a9f2c660edd4e40
Yongho Kim <kyhcraft@gmail.com>
2024-11-05 18:24:59 +0900

    💄 design: 레이아웃 정렬

--- src/widgets/search/SearchBar.tsx
+++ src/widgets/search/SearchBar.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function SearchBar() {
     return (
         <section className="flex gap-5 pt-6">
-            <Search className="" />
+            <Search className="flex-1 min-w-0" />
             <Button className="bg-violet-600 text-white text-[0.8rem] px-6 flex items-center justify-center gap-1">
                 <Image src={Plus} width={12} height={12} alt="Plus" />
                 추가하기
