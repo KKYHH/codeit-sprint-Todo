3fe19c11fb5b17d9d74bb13e3631f23e9a0eae27
Yongho Kim <kyhcraft@gmail.com>
2024-11-05 15:25:49 +0900

    ✨ feat: 나눔스퀘어 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,26 @@
+import '@devShared/styles/index.css';
+import localFont from 'next/font/local';
+
+const nanumRegular = localFont({
+    src: '../../public/fonts/NanumSquareR.otf',
+    variable: '--font-nanum-rg',
+});
+
+const nanumBold = localFont({
+    src: '../../public/fonts/NanumSquareB.otf',
+    variable: '--font-nanum-bd',
+});
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="ko">
+        <html
+            lang="ko"
+            className={`${nanumRegular.className} ${nanumBold.className}`}
+        >
             <body>{children}</body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import '@devShared/styles/index.css';
-
 export default function Page() {
     return (
         <>
             <h1 className="flex text-red-700">Hello, Next.js!</h1>
             <h1 className="flex text-red-700">Hello, Next.js!</h1>
             <h1 className="flex text-red-700">Hello, Next.js!</h1>
+            <h1 className="bold">안녕하세요</h1>
+            <h1 className="bold">안녕하세요</h1>
+            <h1>안녕하세요</h1>
+            <h1>안녕하세요</h1>
+            <h1 className=" text-[5rem]">안녕하세요</h1>
         </>
     );
 }
