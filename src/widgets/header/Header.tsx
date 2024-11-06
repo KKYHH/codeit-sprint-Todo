aa28b8d27de04f337a681b25117b9789a793b19c
Yongho Kim <kyhcraft@gmail.com>
2024-11-06 13:42:07 +0900

    ✨ feat: 로고 클릭 새로고침 이동

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,11 +1,24 @@
+'use client';
+
 import Logo from '@devShared/images/Logo.png';
 import Image from 'next/image';
 
 export default function Header() {
+    const handleRefreshMove = () => {
+        window.location.href = '/';
+    };
+
     return (
         <section className="bg-white shadow-sm w-full p-2">
             <article>
-                <Image src={Logo} alt="Logo" width={150} height={150} />
+                <Image
+                    src={Logo}
+                    alt="Logo"
+                    width={150}
+                    height={150}
+                    onClick={handleRefreshMove}
+                    className="cursor-pointer"
+                />
             </article>
         </section>
     );
